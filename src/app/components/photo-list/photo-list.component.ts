e7535b072a8dc5b120dff174c9f4d35aacfd39e2
gabriel <gabriellopes112009@gmail.com>
2021-06-29 07:18:45 -0300

    loading spinner font awesonme photo-list

--- src/app/components/photo-list/photo-list.component.ts
+++ src/app/components/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { Photo } from "src/app/shared/components/photo-board/interfaces/photo";
 import { PhotoBoardService } from "src/app/shared/components/photo-board/services/photo-board.service";
+import {faCircleNotch} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: 'app-photo-list',
@@ -13,7 +14,11 @@ import { PhotoBoardService } from "src/app/shared/components/photo-board/service
 export class PhotoListComponent implements OnInit{
     title = 'Angular testing';
     public photos$: Observable<Photo[]>;
-  
+    
+    public fa = {
+      faCircleNotch
+    };
+
     constructor(private service: PhotoBoardService){
       
     }
