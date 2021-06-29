d6b2c40a96b19640bda83c9d995bd3466ef0b25b
gabriel <gabriellopes112009@gmail.com>
2021-06-29 08:12:07 -0300

    uso de httpController para verificar requisiçoes do httpTestModule

--- src/app/shared/components/photo-board/services/photo-board.service.spec.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -2,6 +2,21 @@ import { TestBed } from "@angular/core/testing";
 import { PhotoBoardService } from "./photo-board.service";
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 
+const mockData = {
+    api: 'http://localhost:3000/photos',
+    data: [
+        {
+            id: 1,
+            description: 'example 1',
+            src: ''
+        },
+        {
+            id: 2,
+            description: 'example 2',
+            src: ''
+        }
+    ]
+}
 describe(PhotoBoardService.name, () => {
     let service: PhotoBoardService;
     let httpController: HttpTestingController;
@@ -11,7 +26,25 @@ describe(PhotoBoardService.name, () => {
             providers: [PhotoBoardService],
             imports: [HttpClientTestingModule],
         })
+        service = TestBed.inject(PhotoBoardService);
+        httpController = TestBed.inject(HttpTestingController)
     })
-    service = TestBed.inject(PhotoBoardService);
-    httpController = TestBed.inject(HttpTestingController)
+ 
+    afterEach(() => {
+        httpController.verify()
+    })
+    it(`#${PhotoBoardService.prototype.getPhotos.name} should return 
+        photos with description in uppercase`, done => {
+            
+            service.getPhotos().subscribe(
+                photos => {
+                    expect(photos[0].description).toBe('EXAMPLE 1');
+                    expect(photos[1].description).toBe('EXAMPLE 2');
+                    done()
+                }
+            );
+            httpController.expectOne(mockData.api)
+                .flush(mockData.data)
+            
+        })
 })
